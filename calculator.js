0cb89361856a544a4e258927dea3938cfbb2e950
TheDjBor <131305194+TheDjBor@users.noreply.github.com>
2023-07-27 15:50:03 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -5,12 +5,22 @@ const kWhWind = 0.0120;
 const kmTruck = 0.000269;
 const kmAirplane = 0.000253;
 
+// Emission factors for commuting
+const kmDieselCar = 2.7 / 15; // kg CO2 per km
+const kmPetrolCar = 2.3 / 15; // kg CO2 per km
+const kmCar = (kmDieselCar + kmPetrolCar) / 2; // average
+
 function calculateEmissions() {
     const energySource = document.getElementById('energy-source').value;
     const kwh = document.getElementById('kwh').value;
 
     const transportationType = document.getElementById('transportation-type').value;
     const distance = document.getElementById('distance').value;
+
+    // New calculations for commuting
+    const days = document.getElementById('days').value;
+    const employees = document.getElementById('employees').value;
+    const commuteDistance = document.getElementById('commute-distance').value;
 
     let emissionEnergy;
     switch (energySource) {
@@ -39,12 +49,17 @@ function calculateEmissions() {
             emissionTransportation = 0;
     }
 
+    let totalCommuteDistance = days * employees * commuteDistance;
+    let emissionCommute = totalCommuteDistance * kmCar;
+
     document.getElementById('result-energy-daily').textContent = `Daily emission from energy usage: ${emissionEnergy.toFixed(2)} kg`;
     document.getElementById('result-energy-weekly').textContent = `Weekly emission from energy usage: ${(emissionEnergy * 7).toFixed(2)} kg`;
     document.getElementById('result-energy-monthly').textContent = `Monthly emission from energy usage: ${(emissionEnergy * 30).toFixed(2)} kg`;
     document.getElementById('result-energy-yearly').textContent = `Yearly emission from energy usage: ${(emissionEnergy * 365).toFixed(2)} kg`;
 
-    document.getElementById('result-transportation').textContent = `Emission from transportation: ${emissionTransportation.toFixed(2)} kg`;
+    document.getElementById('result-transportation').textContent = `Emission from product transportation: ${emissionTransportation.toFixed(2)} kg`;
+
+    document.getElementById('result-commute').textContent = `Emission from commuting: ${emissionCommute.toFixed(2)} kg`;
 
     document.getElementById('download').style.display = 'block';
 }
@@ -59,7 +74,7 @@ function generatePDF() {
 
     const energySource = document.getElementById('energy-source').value;
     doc.text(20, 30, `Energy Source: ${energySource}`);
-    
+
     const kwh = document.getElementById('kwh').value;
     doc.text(20, 40, `Energy Usage (Daily kWh): ${kwh}`);
 
@@ -74,9 +89,11 @@ function generatePDF() {
     doc.text(20, 90, document.getElementById('result-energy-monthly').textContent);
     doc.text(20, 100, document.getElementById('result-energy-yearly').textContent);
     doc.text(20, 110, document.getElementById('result-transportation').textContent);
+    doc.text(20, 120, document.getElementById('result-commute').textContent);
 
     doc.save('carbon_usage_report.pdf');
 }
 
 document.getElementById('calculate').addEventListener('click', calculateEmissions);
 document.getElementById('download').addEventListener('click', generatePDF);
+d('download').addEventListener('click', generatePDF);
